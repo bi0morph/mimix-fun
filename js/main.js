fafa19a272b7b67a00b8c80edcaeded03d09a54c
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-09-23 13:00:07 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rus on 23.09.2015.
+ */
+(function() {
+  if ( mimic && mimic.init ) {
+    mimic.init();
+  } else {
+    console.error('mimic is not include');
+  }
+})();
---
+++ js/mimic/mimic.js
@@ -0,0 +1,11 @@
+/**
+ * Created by rus on 23.09.2015.
+ */
+(function() {
+  'use strict';
+
+  function Mimic(canvasId) {
+
+  }
+    app.mimic = Mimic('#constructor_canvas');
+})();
