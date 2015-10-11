a81ddd59ffbba024eabd4d6362b549b08d3a055f
Ruslan Ustiuhov <rusustyugov@gmail.com>
2015-10-11 15:37:11 +0300

    Add cursor observer

---
+++ js/mimic/cursor.js
@@ -0,0 +1,21 @@
+/**
+ * Created by rus on 11.10.2015.
+ */
+(function(global) {
+	"use strict";
+
+	var CursorState = fabric.util.createClass( {
+		initialize: function () {},
+		state: null
+	});
+	fabric.util.object.extend(CursorState.prototype, fabric.Observable);
+
+	global.mimic = global.mimic || {};
+	global.mimic.cursor = new CursorState();
+	global.mimic.cursor.on('cursor:change', function(type) {
+		if(type) {
+			console.log('cursor:change', type);
+			this.state = type;
+		}
+	})
+})(window);
